23e67d7a13f427b7ba5732eaf91f5e65ba487501
rkutsel <roman.kutsel@docusign.com>
2022-07-25 12:11:02 -0700

    code formatting

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -11,56 +11,56 @@ const resolvers = {
 			throw new AuthenticationError("You need to be logged in!");
 		},
 	},
-	Mutation: {
-		addUser: async (parent, { username, email, password }) => {
-			const user = await User.create({ username, email, password });
-			const token = signToken(user);
-			return { token, user };
-		},
-		login: async (parent, { email, password }) => {
-			const user = await User.findOne({ email });
+	// Mutation: {
+	// 	addUser: async (parent, { username, email, password }) => {
+	// 		const user = await User.create({ username, email, password });
+	// 		const token = signToken(user);
+	// 		return { token, user };
+	// 	},
+	// 	login: async (parent, { email, password }) => {
+	// 		const user = await User.findOne({ email });
 
-			if (!user) {
-				throw new AuthenticationError("No user found with this email address");
-			}
+	// 		if (!user) {
+	// 			throw new AuthenticationError("No user found with this email address");
+	// 		}
 
-			const correctPw = await user.isCorrectPassword(password);
+	// 		const correctPw = await user.isCorrectPassword(password);
 
-			if (!correctPw) {
-				throw new AuthenticationError("Incorrect credentials");
-			}
+	// 		if (!correctPw) {
+	// 			throw new AuthenticationError("Incorrect credentials");
+	// 		}
 
-			const token = signToken(user);
+	// 		const token = signToken(user);
 
-			return { token, user };
-		},
-		saveBook: async ({ user, body }, res) => {
-			console.log(user);
-			try {
-				const updatedUser = await User.findOneAndUpdate(
-					{ _id: user._id },
-					{ $addToSet: { savedBooks: body } },
-					{ new: true, runValidators: true }
-				);
-				return res.json(updatedUser);
-			} catch (err) {
-				console.log(err);
-				return res.status(400).json(err);
-			}
-		},
-		removeBook: async ({ user, params }, res) => {
-			const updatedUser = await User.findOneAndUpdate(
-				{ _id: user._id },
-				{ $pull: { savedBooks: { bookId: params.bookId } } },
-				{ new: true }
-			);
-			if (!updatedUser) {
-				return res
-					.status(404)
-					.json({ message: "Couldn't find user with this id!" });
-			}
-			return res.json(updatedUser);
-		},
-	},
+	// 		return { token, user };
+	// 	},
+	// 	saveBook: async ({ user, body }, res) => {
+	// 		console.log(user);
+	// 		try {
+	// 			const updatedUser = await User.findOneAndUpdate(
+	// 				{ _id: user._id },
+	// 				{ $addToSet: { savedBooks: body } },
+	// 				{ new: true, runValidators: true }
+	// 			);
+	// 			return res.json(updatedUser);
+	// 		} catch (err) {
+	// 			console.log(err);
+	// 			return res.status(400).json(err);
+	// 		}
+	// 	},
+	// 	removeBook: async ({ user, params }, res) => {
+	// 		const updatedUser = await User.findOneAndUpdate(
+	// 			{ _id: user._id },
+	// 			{ $pull: { savedBooks: { bookId: params.bookId } } },
+	// 			{ new: true }
+	// 		);
+	// 		if (!updatedUser) {
+	// 			return res
+	// 				.status(404)
+	// 				.json({ message: "Couldn't find user with this id!" });
+	// 		}
+	// 		return res.json(updatedUser);
+	// 	},
+	// },
 };
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,7 +12,7 @@ const typeDefs = gql`
 		_id: ID
 		username: String
 		email: String
-		bookCount: Number
+		bookCount: Int
 		savedBooks: [Book]!
 	}
 	type Book {
