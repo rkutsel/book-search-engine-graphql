bcd124152a262b91638a5179f6ba1792ffc3a54f
rkutsel <roman.kutsel@docusign.com>
2022-07-27 13:19:27 -0700

    heroku deploy changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
+	user: process.env.DB_USER || null,
+	pass: process.env.DB_PASS || null,
 });
 
 module.exports = mongoose.connection;
